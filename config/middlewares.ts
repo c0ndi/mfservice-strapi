d353c8597188f54670f1fedcb4017e31808afa43
Konrad <kowalik0konrad@gmail.com>
2023-04-11 19:50:12 +0200

    uploads fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,12 +9,4 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'https://mfservice.vercel.app/', "http://13.49.159.208:1337"]
-    }
-  },
 ];
